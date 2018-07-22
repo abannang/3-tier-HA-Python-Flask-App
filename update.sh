c1e6a5a94d4333823121ca1c40073550887073a9
Abhiram Annangi <abhiram.annangi@oracle.com>
2018-07-21 18:10:11 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,4 +2,5 @@
 set -e
 sudo apt-get update
 sudo apt-get install python-pip python-dev build-essential -y
+sudo pip install -r ~/3-tier-HA-Python-Flask-App/requirements.txt
 sudo apt-get install unzip -y
