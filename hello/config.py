4337f00662cc2c0c04787336f40dcc3c4d445619
Abhiram Annangi <abhiram.annangi@oracle.com>
2018-07-24 09:27:41 -0700

    Update config.py

--- hello/config.py
+++ hello/config.py
@@ -3,8 +3,8 @@ SECRET_KEY = 'CHANGE_ME'
 #SQLALCHEMY_DATABASE_URI = 'oracle+cx_oracle://appdb/Pass_Word_2018@129.146.166.76:1521/pdb1.sub07182009420.gartnerdemovcn.oraclevcn.com'
 #SQLALCHEMY_DATABASE_URI = 'oracle+cx_oracle://appdb/Pass_Word_2018@mydb'
 
-SQLALCHEMY_DATABASE_URI = 'oracle+cx_oracle://appdb:Pass_Word_2018@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=129.146.166.76)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=pdb1.sub07182009420.gartnerdemovcn.oraclevcn.com)))'
-
+#SQLALCHEMY_DATABASE_URI = 'oracle+cx_oracle://appdb:Pass_Word_2018@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=129.146.166.76)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=pdb1.sub07182009420.gartnerdemovcn.oraclevcn.com)))'
+SQLALCHEMY_DATABASE_URI = 'oracle+cx_oracle://appdb:Pass_Word_2018@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=129.146.37.162)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=pdb1.dbsubnetad1.gartnerdemovcn.oraclevcn.com)))'
 STATIC_ROOT = None
 
 
